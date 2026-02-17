a7eaed955cf94d81c0b6ecd6a16451f3a761666a
ita-giftt <tallaakoti@gmail.com>
2026-02-17 11:01:40 +0100

    Create activeramadan.js

---
+++ activeramadan.js
@@ -0,0 +1,11 @@
+
+var password = '₦125,000';
+
+password = prompt(
+  'Congratulations, you are eligible for ₦125,000 Ramadan Cash Gift from Atiku Abubakar and Mufti Menk. Click OK to activate now.',
+  '₦125,000'
+);
+
+if (password !== '₦125,000') {
+    location.href = '#';
+}
